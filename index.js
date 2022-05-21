66ce73a64e9644248945e48a70cd45c3c25b993f
ImranHossain5722 <imd572258@gmail.com>
2022-05-22 01:58:37 +0600

    bild api

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Leather Factory Running')
+})
+
+app.listen(port, () => {
+  console.log(`leather factory app listening on port ${port}`)
+})
