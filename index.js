42e1807dddde4106dceac9872242d4f21b560c01
ImranHossain5722 <imd572258@gmail.com>
2022-05-23 03:11:46 +0600

    build first api for manufacturer all aitems

--- index.js
+++ index.js
@@ -12,13 +12,24 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u2chb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ehyud.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run (){
 try{
  await client.connect()
- 
+  const itemsCollection = client.db('lather_database').collection('manufacturerItems')
+
+// Api for my maunfacturer Items
+app.get('/manufacturerItems', async(req,res)=>{
+
+  const query ={}
+  const cursor = itemsCollection.find(query)
+  const items = await cursor.toArray()
+  res.send(items)
+
+})
+
 
 
 }
